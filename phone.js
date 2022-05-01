3a3c053a7a4e37035f09c667d9687552e03d39f9
Schyler27 <92312027+Schyler27@users.noreply.github.com>
2022-05-01 17:49:50 +0530

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,45 @@
+status = "";
+phone_image = "";
+objects = [];
+
+function preload(){
+    phone_image = loadImage("phone.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(600 , 350);
+    canvas.center();
+    object_Detector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    object_Detector.detect(phone_image , gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(phone_image , 0 , 0 , 640 , 350);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x, objects[i].y);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
